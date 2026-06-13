a61c4ddc10d8f1be2e8c6cae681ef0cb7bd48b97
TAEYEONnn <bis08@hanmail.net>
2026-06-13 22:56:00 +0900

    개인정보 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ["192.168.10.46"],
+    allowedDevOrigins: ["192.168.10.46","192.168.137.1"],
 
 };
 
