4718693f98f8bc613a8b49bc657d90753fff61ef
TAEYEONnn <bis08@hanmail.net>
2026-06-14 10:50:45 +0900

    개발자 키 추가

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const allowedDevOrigins = process.env.ALLOWED_DEV_ORIGINS
+  ? process.env.ALLOWED_DEV_ORIGINS.split(",")
+  : undefined;
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins,
+};
 
 export default nextConfig;
